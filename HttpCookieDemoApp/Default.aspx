0fdcdd60906b427a3e6cf47f79657b0eafdb0f72
Shi Hao Wang <txstudio@outlook.com>
2018-09-13 16:47:11 +0800

    Initial commit

---
+++ HttpCookieDemoApp/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        #description {
+            line-height: 30px;
+            background: #eee;
+            padding-top: 20px;
+            padding-bottom: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <ul id="description">
+            <li>取得 Cookie 資料要使用 Request.Cookie</li>
+            <li>設定 Cookie 資料要使用 Response.Cookie.Add( ... )</li>
+        </ul>
+    </div>
+    <form id="form1" runat="server">
+        <div>
+            Cookie Value : 
+            <asp:Literal runat="server" ID="CookieValueLiteral"></asp:Literal>
+
+            <hr />
+
+            Set Cookie Value :
+            <asp:TextBox runat="server" ID="CookieValueTextBox"></asp:TextBox>
+
+            <asp:Button runat="server" ID="GenerateCookieButton" 
+                        Text="Set Value"
+                        OnClick="GenerateCookieButton_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ HttpCookieDemoApp/Default.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    /*
+     * 取得 Cookie 資料要使用 Request.Cookie
+     * 
+     * 設定 Cookie 資料要使用 Response.Cookie.Add( ... )
+     */
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.DisplayCookieValue();
+    }
+
+    protected void GenerateCookieButton_Click(object sender, EventArgs e)
+    {
+        string _value;
+
+        _value = this.CookieValueTextBox.Text;
+
+        if (string.IsNullOrWhiteSpace(_value) == true)
+            return;
+
+        this.CookieValue = _value;
+
+        this.DisplayCookieValue();
+    }
+
+    private void DisplayCookieValue()
+    {
+        this.CookieValueLiteral.Text = this.CookieValue;
+    }
+
+
+    const string CookieValueName = "_cookie_value";
+
+    private string CookieValue
+    {
+        get
+        {
+            if (Request.Cookies[CookieValueName] == null)
+                return string.Empty;
+
+            return Request.Cookies[CookieValueName].Value;
+        }
+        set
+        {
+            HttpCookie _cookie;
+            
+            if(Request.Cookies[CookieValueName] == null)
+            {
+                _cookie = new HttpCookie(CookieValueName);
+                _cookie.HttpOnly = true;
+            }
+            else
+            {
+                _cookie = Request.Cookies[CookieValueName];
+            }
+
+            _cookie.Value = value;
+
+            //設定 HttpCookie 需要使用 Response.Cookie.Add ...
+            Response.Cookies.Add(_cookie);
+        }
+    }
+
+}
